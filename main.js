a1d31ab1f8f9a5530903f3420e67d207d15a2e9d
Samandar Yakhyaev <104832305+yakhyaevs@users.noreply.github.com>
2022-05-22 15:05:46 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+//Function exercises
+const calculateWeight = (earthWeight, planet) => {
+    switch (planet) {
+        case 'Mercury':
+            return earthWeight * .378;
+        case 'Venus':
+            return earthWeight * .907;
+        case 'Mars':
+            return earthWeight * .377;
+        case 'Jupiter':
+            return earthWeight * 2.36;
+        case 'Saturn':
+            return earthWeight * .916;
+        default:
+            return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.'
+    }
+}
+/*
+// As a function declaration: 
+function calculateWeight(earthWeight, planet) {
+    switch (planet) {
+        case 'Mercury':
+            return earthWeight * .378;
+        case 'Venus':
+            return earthWeight * .907;
+        case 'Mars':
+            return earthWeight * .377;
+        case 'Jupiter':
+            return earthWeight * 2.36;
+        case 'Saturn':
+            return earthWeight * .916;
+	 default:
+		return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.'
+    }
+}
+*/
+console.log(calculateWeight(100, 'Jupiter'))
